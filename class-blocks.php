22b53b2e75b071fcb7ac2478a79dd8df026111e4
Seong <sryoo6@gatech.edu>
2022-03-15 23:25:46 -0400

    First commit with hero block and without src features

---
+++ class-blocks.php
@@ -0,0 +1,48 @@
+<?php
+
+include( plugin_dir_path( __FILE__ ) . 'block-rendering/hero.php' );
+
+class UrbanHeatATL_Blocks {
+  const STYLES = array(
+    'admin',
+    'hero',
+  );
+  const SCRIPTS = array(
+    'blocks',
+  );
+  const DEPENDENCIES = array(
+    'wp-blocks',
+    'wp-i18n',
+    'wp-editor',
+    'wp-date',
+  );
+  function actions__editor() {
+    add_action( 'enqueue_block_editor_assets', function() {
+      $styles = constant( __CLASS__ . '::STYLES' );
+      $scripts = constant( __CLASS__ . '::SCRIPTS' );
+      $dependencies = constant( __CLASS__ . '::DEPENDENCIES' );
+      $style_url = plugin_dir_url( __FILE__ ) . '/editor/css/';
+      $script_url = plugin_dir_url( __FILE__ ) . '/editor/js/';
+      foreach ( $styles as $style ) {
+        wp_enqueue_style(
+          'urbanheat-' . $style, 
+          $style_url . $style . '.css' 
+        );
+      }
+      foreach ( $scripts as $script ) {
+        wp_enqueue_script(
+          'urbanheat-' . $script,
+          $script_url . $script . '.js',
+          $dependencies
+        );
+      }
+    } );
+  }
+  function filters__editor() {
+    
+  }
+  function __construct() {
+    $this->actions__editor();
+    $this->filters__editor();
+  }
+}
---
+++ urbanheat-blocks.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: UrbanHeatATL blocks
+ */
+
+include( plugin_dir_path( __FILE__ ) . 'class-blocks.php' );
+$uha_blocks = new UrbanHeatATL_Blocks();
